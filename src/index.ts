b3fabe8c397731d859768b672b88ee1c0f57cd87
halvaradop_ <hernanvid123@gmail.com>
2024-05-22 13:04:26 -0500

    chore: create utilities

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import plugin from "tailwindcss/plugin"
+import { PluginCreator } from "tailwindcss/types/config"
+
+const tags = [
+    "div",
+    "span",
+    "a",
+    "p",
+    "img",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "ul",
+    "ol",
+    "li",
+    "table",
+    "tr",
+    "td",
+    "form",
+    "input",
+    "button",
+    "section",
+    "main",
+    "body",
+    "article",
+    "label",
+    "img",
+    "figure",
+    "picture",
+    "caption",
+    "footer"
+]
+
+export const creator: PluginCreator = (configApi) => {
+    const { addVariant } = configApi
+    tags.forEach(tag => addVariant(tag, `:where(&:is(${tag}), & > ${tag})`))
+}
+
+export default plugin(creator)
