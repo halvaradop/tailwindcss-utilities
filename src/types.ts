ca93d371d159495f81c5db66e95f2601c727c1fa
halvaradop_ <hernanvid123@gmail.com>
2024-05-25 16:11:55 -0500

    docs: add documentation to the type file

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
-
+/**
+ * This file contains the types and interfaces used throughout the application. 
+ * It allows us to define the types required for the different utilities.
+ */
 export type FontFluency = Pick<CSSStyleDeclaration, "fontSize" | "lineHeight" | "letterSpacing">
 export type EntryCSS<Key = string, Value = string> = Key extends string ? Record<string, Value> : Record<keyof Key, Value>
