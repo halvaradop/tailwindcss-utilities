bf436b16ffa8bd482fe19835f666333f01c0725d
halvaradop_ <hernanvid123@gmail.com>
2024-05-25 16:06:57 -0500

    docs: add documentation to verifySelectorsTheme function

--- src/utils.ts
+++ src/utils.ts
@@ -2,10 +2,19 @@ import { InvalidSelectorError } from "./errors"
 
 export const tagRegex = /^([a-z]{1,}\-?){1,}$/
 
+/**
+ * Verifies that the tags/selectors provided by the user in the `theme` property 
+ * of the TailwindCSS configuration have a correct structure or name. 
+ * They must match the regex `/^([a-z]{1,}\-?){1,}$/`.
+ * 
+ * @param selectors List of selectors provided by the user
+ * @returns The selectors provided by the user, or throws an error if any selector is invalid
+ * @throws {InvalidSelectorError} If any selector is invalid
+ */
 export const verifySelectorsTheme = (selectors: string[] = []): string[] => {
     const haveInvalid = selectors.some(selector => !tagRegex.test(selector))
     if(haveInvalid) {
-        throw new InvalidSelectorError("Invalid tag html, you must the verify the tags")
+        throw new InvalidSelectorError("Invalid HTML tag. Please verify the tags.");
     }
     return selectors
 }
