ce43ac04108e732f2cebb02fb0824ab7c57710e7
halvaradop_ <hernanvid123@gmail.com>
2024-07-07 16:34:42 -0500

    feat: create button component

--- src/page/src/lib/@types/props.ts
+++ src/page/src/lib/@types/props.ts
@@ -1,4 +1,9 @@
+import { ArgsFunction } from "./types"
+import { ButtonHTMLAttributes } from "react"
+import { VariantProps } from "class-variance-authority"
 
 export interface LayoutProps {
     children: React.ReactNode
 }
+
+export type ButtonProps<T extends ArgsFunction> = ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<T>
