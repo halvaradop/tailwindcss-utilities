b34fc03fc4a47515852b6e043cb8e386dd89b00c
halvaradop_ <hernanvid123@gmail.com>
2024-07-07 16:28:08 -0500

    chore: install depedencies and set colors:
    - Configure colors using tailwindcss configuration file
    - Install @halvaradop/tailwindcss-utilities and class-variance-authority
      packages

--- src/page/tailwind.config.ts
+++ src/page/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import utilities from "@halvaradop/tailwindcss-utilities"
 
 const config: Config = {
   content: [
@@ -7,8 +8,24 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          DEFAULT: "#101111"
+        },
+        gray: {
+          DEFAULT: "#C0C0C0",
+          100: "#262626",
+          200: "#1B1B1B",
+        },
+        orange: {
+          DEFAULT: "#F78C6C"
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    utilities
+  ],
 };
 export default config;
