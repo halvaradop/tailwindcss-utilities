9e586880339adb8cbe00d95451581342a01b93b5
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-07-16 16:57:39 -0500

    feat: add styles for main section for devices

--- app/src/lib/@types/props.ts
+++ app/src/lib/@types/props.ts
@@ -3,9 +3,13 @@ import { ButtonHTMLAttributes } from "react"
 import { VariantProps } from "class-variance-authority"
 
 export interface LayoutProps {
-    children: React.ReactNode
+    children: React.ReactNode,
+    className?: string
 }
 
 export type ButtonProps<T extends ArgsFunction> = ButtonHTMLAttributes<HTMLButtonElement> & VariantProps<T>
 
-export interface CardUtilityProps extends LayoutProps {}
+export interface CardUtilityProps extends LayoutProps {
+    href?: string,
+    classNameCard?: string
+}
