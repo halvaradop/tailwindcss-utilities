94b58c937c44db5bcddd1be6099a9810a593c758
Hernan Alvarado <105169289+halvaradop@users.noreply.github.com>
2024-07-16 17:32:43 -0500

    feat: add styles for footer section for devices

--- app/src/ui/footer.tsx
+++ app/src/ui/footer.tsx
@@ -8,14 +8,14 @@ export const Footer = () => {
 
     return (
         <footer className="pt-10 pb-8">
-            <section className="w-11/12 mx-auto grid grid-cols-2 items-start text-gray-50">
-                <ul className="flex flex-col text-lg font-medium li:my-2">                    
+            <section className="w-11/12 mx-auto grid grid-cols-2 items-start text-gray-50 base:w-10/12 lg:grid-cols-3 lg:items-center lg:justify-between lg:max-w-screen-xl">
+                <ul className="flex flex-col text-lg font-medium li:my-2 lg:flex-row lg:li:mx-5">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/">Installation</Link></li>
                     <li><Link href="/">Docs</Link></li>
                 </ul>
-                <figure className="ml-auto flex items-center gap-x-5">
-                    <Image src={moon} alt="moon icon" />
+                <figure className="ml-auto flex items-center gap-x-5 lg:order-1">
+                    <Image className="hover:cursor-pointer" src={moon} alt="moon icon" />
                     <span>|</span>
                     <Link href="https://www.npmjs.com/package/@halvaradop/tailwindcss-utilities" target="_blank">
                         <Image src={npm} alt="npm icon" />
@@ -24,7 +24,7 @@ export const Footer = () => {
                         <Image src={github} alt="github icon" />
                     </Link>
                 </figure>
-                <p className="mt-8 col-span-2 text-center">@2024, TailwindCSS Utilities</p>
+                <p className="mt-8 col-span-2 text-center lg:col-span-1 lg:m-0">&#169;​ 2024, TailwindCSS Utilities</p>
             </section>
         </footer>
     )
