159a48b53ea9c69cb2f75093773e182469b5cc4f
halvaradop_ <hernanvid123@gmail.com>
2024-08-06 17:44:39 -0500

    fix: correct `min-width` utility implementation

--- src/utilities/min-width-utility.ts
+++ src/utilities/min-width-utility.ts
@@ -1,17 +1,13 @@
-import { Config } from "tailwindcss";
+import type { KeyValuePair, ResolvableTo } from "tailwindcss/types/config"
 
 /**
  * Defines default values for the minWidth theme in TailwindCSS.
  * It provides a set of predefined minimum widths.
  */
-export const minWidthUtilities: Partial<Config> = {
-    theme: {
-        minWidth: {
-            sm: "640px",
-            md: "768px",
-            lg: "1024px",
-            xl: "1280px",
-            "2xl": "1526px"
-        }
-    }
+export const minWidthUtilities: ResolvableTo<KeyValuePair<string, string>> = {
+    "sm": "640px",
+    "md": "768px",
+    "lg": "1024px",
+    "xl": "1280px",
+    "2xl": "1526px"
 }
