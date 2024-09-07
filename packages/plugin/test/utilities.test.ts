3a56ab4ea66b4a58cc4d8de6e86ace1b942643d5
halvaradop <hernanvid123@gmail.com>
2024-09-06 19:14:46 -0500

    chore: 1

--- packages/plugin/test/utilities.test.ts
+++ packages/plugin/test/utilities.test.ts
@@ -1,10 +1,1 @@
-import postcss from "postcss"
-import tailwindcss from "tailwindcss"
-import { describe, expect, test } from "vitest"
 import { generateClasses } from "./utils"
-
-describe("Plugin Class Generation", () => {
-    test("should generate correct classes", async () => {
-        generateClasses(`<div class=""></div>`)
-    })
-})
