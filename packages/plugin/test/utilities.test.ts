071a8381970a08b8b768d18de47895740c4c4690
halvaradop <hernanvid123@gmail.com>
2024-09-06 19:17:22 -0500

    chore: setup vitest

--- packages/plugin/test/utilities.test.ts
+++ packages/plugin/test/utilities.test.ts
@@ -1,1 +1,9 @@
 import { generateClasses } from "./utils"
+import { describe, test, expect } from "vitest"
+
+describe("Plugin", () => {
+    test("generateClasses", async () => {
+        const classes = await generateClasses("scroll")
+        expect(classes).toBe("")
+    })
+})
