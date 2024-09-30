73d120528f4c663db960b111e58bbc904ba40afa
Hernan Alvarado <hernanvid123@gmail.com>
2024-09-29 22:11:22 -0500

    chore: update test to use `toMatch` instead of `toContain` (#67)

--- packages/tailwindcss-utilities/test/index.test.ts
+++ packages/tailwindcss-utilities/test/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, test } from "vitest"
 import { extractClasses } from "@halvaradop/tailwindcss-core/utils"
-import plugin from "../src/index"
+import plugin from "../src"
 
 const generateClasses = extractClasses(plugin)
 
@@ -8,36 +8,36 @@ describe("scroll utilities", () => {
     test.concurrent("generate the css for scroll utilities", async ({ expect }) => {
         const html = `<div class="scroll:w-2"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".scroll\\:w-2::-webkit-scrollbar{width:0.5rem}")
+        expect(css).toMatch(".scroll\\:w-2::-webkit-scrollbar{width:0.5rem}")
     })
 
     test.concurrent("generate the css for scroll utilities with custom width", async ({ expect }) => {
         const html = `<div class="scroll:w-[10px]"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".scroll\\:w-\\[10px\\]::-webkit-scrollbar{width:10px}")
+        expect(css).toMatch(".scroll\\:w-\\[10px\\]::-webkit-scrollbar{width:10px}")
     })
 
     test.concurrent("generate the css for scroll thumb utilities", async ({ expect }) => {
         const html = `<div class="thumb:my-4"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".thumb\\:my-4::-webkit-scrollbar-thumb{margin-top:1rem;margin-bottom:1rem}")
+        expect(css).toMatch(".thumb\\:my-4::-webkit-scrollbar-thumb{margin-top:1rem;margin-bottom:1rem}")
     })
 
     test.concurrent("generate the css for scroll thumb utilities with custom margin", async ({ expect }) => {
         const html = `<div class="thumb:my-[8px]"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".thumb\\:my-\\[8px\\]::-webkit-scrollbar-thumb{margin-top:8px;margin-bottom:8px}")
+        expect(css).toMatch(".thumb\\:my-\\[8px\\]::-webkit-scrollbar-thumb{margin-top:8px;margin-bottom:8px}")
     })
 
     test.concurrent("generate the css for scroll track utilities", async ({ expect }) => {
         const html = `<div class="track:border"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".track\\:border::-webkit-scrollbar-track{border-width:1px}")
+        expect(css).toMatch(".track\\:border::-webkit-scrollbar-track{border-width:1px}")
     })
 
     test.concurrent("generate the css for scroll track utilities with custom border width", async ({ expect }) => {
         const html = `<div class="track:border-[3px]"></div>`
         const css = await generateClasses(html)
-        expect(css).toContain(".track\\:border-\\[3px\\]::-webkit-scrollbar-track{border-width:3px}")
+        expect(css).toMatch(".track\\:border-\\[3px\\]::-webkit-scrollbar-track{border-width:3px}")
     })
 })
