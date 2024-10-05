4ce278f5bf80a6c16e1bf4cfba20991b90833ef1
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-05 18:17:20 -0500

    chore: update `vitest.config.ts` file

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vitest/config"
  */
 export default defineConfig({
     test: {
-        include: ["packages/**/*.test.ts"],
+        include: ["test/**/*.test.ts"],
         coverage: {
             provider: "v8",
             reporter: ["text", "html"],
