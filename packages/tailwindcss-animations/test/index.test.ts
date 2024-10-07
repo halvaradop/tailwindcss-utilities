2a35e9e36c8b1364e9146aa0d7cc36c6c08cffd5
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-07 11:50:12 -0500

    chore: add tests to check the theme config

--- packages/tailwindcss-animations/test/index.test.ts
+++ packages/tailwindcss-animations/test/index.test.ts
@@ -1,7 +1,8 @@
 import { describe, test, expect } from "vitest"
 import { extractClasses } from "@halvaradop/tailwindcss-core/utils"
-import plugin from "../src/index.js"
+import { plugin } from "../src/index.js"
 import { toSlashCase } from "../src/utils.js"
+import { theme } from "../src/theme.js"
 
 export const generateClasses = extractClasses(plugin)
 
@@ -11,7 +12,13 @@ describe("Plugin", () => {
             const html = `<div class="animation-delay-100"></div>`
             const css = await generateClasses(html)
             expect(css).toMatch(".animation-delay-100{animation-delay:100ms}")
+            expect(css).not.toMatch(".animation-delay-200{animation-delay:200ms}")
         })
+    })
+
+    test("Loaded keyframes utility classes", async () => {
+        expect(plugin.config?.theme?.keyframes).toEqual(theme.keyframes)
+        expect(plugin.config?.theme?.keyframes).not.toEqual({})
     })
 
     describe("Conflict prevention", () => {
@@ -49,6 +56,31 @@ describe("Animate delay utility classes", () => {
 
     testCases.forEach(({ className, expected }) => {
         test.concurrent(`animation delay with class ${className}`, async () => {
+            const html = `<div class="${className}"></div>`
+            const css = await generateClasses(html)
+            expect(css).toMatch(expected)
+        })
+    })
+})
+
+describe("Keyframes utility classes", () => {
+    const testCases = [
+        {
+            className: "animate-fade-in",
+            expected: "@keyframes fade-in{0%{opacity:0}100%{opacity:1}}",
+        },
+        {
+            className: "animate-fade-out",
+            expected: "@keyframes fade-out{0%{opacity:1}100%{opacity:0}}",
+        },
+        {
+            className: "animate-rotate-90",
+            expected: "@keyframes rotate-90{0%{transform:rotate(0deg)}100%{transform:rotate(90deg)}}",
+        },
+    ]
+
+    testCases.forEach(({ className, expected }) => {
+        test.concurrent(`keyframes with class ${className}`, async () => {
             const html = `<div class="${className}"></div>`
             const css = await generateClasses(html)
             expect(css).toMatch(expected)
