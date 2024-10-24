d2fd224500e932ce5b589c7c402539c1123b656a
Hernan Alvarado <hernanvid123@gmail.com>
2024-10-24 13:55:33 -0500

    chore: add merge function with tests (#81)
    - Implement a `merge` function to combine two objects
    - Add unit tests to `merge` function

--- packages/tailwindcss-core/test/index.test.ts
+++ packages/tailwindcss-core/test/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest"
-import { extractClasses } from "../src/generate-classes"
+import { extractClasses, merge } from "../src/generate-classes"
 
 /**
  * Provide a mock implementation of the extractClasses function with an
@@ -29,4 +29,49 @@ describe("Extract classes from tailwindcss", () => {
         const css = await generateClasses(html)
         expect(css).toMatch(".text-lg{font-size:1.125rem;line-height:1.75rem}")
     })
+})
+
+describe("Merge objects", () => {
+    test("Merge two objects", () => {
+        const source = { a: 1, b: 2 }
+        const target = { b: 3, c: 4 }
+        expect(merge(source, target)).toEqual({ a: 1, b: 3, c: 4 })
+    })
+
+    test("Merge two objects with nested objects", () => {
+        const source = { a: 1, b: { c: 2 } }
+        const target = { b: 3, c: 4 }
+        expect(merge(source, target)).toEqual({ a: 1, b: { c: 2 }, c: 4 })
+    })
+
+    test("Merge two objects with nested objects and nested objects", () => {
+        const source = { a: 1, b: { c: 2 } }
+        const target = { b: { c: 3 }, c: 4 }
+        expect(merge(source, target)).toEqual({ a: 1, b: { c: 3 }, c: 4 })
+    })
+
+    test("Merge two objects with nested objects and nested objects with different keys", () => {
+        const source = { a: 1, b: { c: 2 } }
+        const target = { b: { d: 3 }, c: 4 }
+        expect(merge(source, target)).toEqual({ a: 1, b: { c: 2, d: 3 }, c: 4 })
+    })
+
+    test("Merge two objects with nested objects and nested objects with different keys and nested objects", () => {
+        const source = { a: 1, b: { c: 2, d: { e: 3 } } }
+        const target = { b: { d: 4 }, c: 5 }
+        expect(merge(source, target)).toEqual({ a: 1, b: { c: 2, d: { e: 3 } }, c: 5 })
+    })
+
+    test("Merge two objects with nested objects and nested objects with different keys and nested objects with different keys", () => {
+        const source = { a: 1, b: { c: 2, d: { e: 3 } } }
+        const target = { b: { d: { f: 4 } }, c: 5 }
+        expect(merge(source, target)).toEqual({ a: 1, b: { c: 2, d: { e: 3, f: 4 } }, c: 5 })
+    })
+
+    test("Merge two object with nested object and nested object without priority", () => {
+        const source = { a: 1, b: { c: 2, d: { e: 3 } } }
+        const target = { b: { d: 4 }, c: 5 }
+        expect(merge(source, target, false)).toEqual({ a: 1, b: { c: 2, d: 4 }, c: 5 })
+    })
+
 })
