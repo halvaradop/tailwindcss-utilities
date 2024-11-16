ff4c025f5dd5bf111c96b518566e0d5abce98d14
Hernan Alvarado <hernanvid123@gmail.com>
2024-11-16 11:40:42 -0500

    chore: exclude `app` folter from tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vitest/config"
  */
 export default defineConfig({
     test: {
+        exclude: ["**/node_modules", "**/dist", "app"],
         coverage: {
             provider: "v8",
             reporter: ["text", "html"],
