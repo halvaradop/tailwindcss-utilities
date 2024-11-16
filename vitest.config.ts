1b5a32e28835da6f9408d76bcca7621cb17bc355
Hernan Alvarado <hernanvid123@gmail.com>
2024-11-16 11:52:27 -0500

    chore: update `ci.yaml` workflow file

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from "vitest/config"
  */
 export default defineConfig({
     test: {
-        exclude: ["**/node_modules", "**/dist", "app"],
         coverage: {
             provider: "v8",
             reporter: ["text", "html"],
