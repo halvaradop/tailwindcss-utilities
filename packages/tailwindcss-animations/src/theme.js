cbadabff53c3b86397a06bd58dbbb18967ef4f44
Hernan Alvarado <hernanvid123@gmail.com>
2025-04-22 12:29:35 -0500

    chore(animations): add `animation-` utilities
    - `animation-count-`
    - `animation-fill-mode-`
    - `animation-range-`
    - `animation-timeline-`

--- packages/tailwindcss-animations/src/theme.js
+++ packages/tailwindcss-animations/src/theme.js
@@ -1,28 +1,4 @@
 export const theme = {
-    animationIterationCount: {
-        none: "0",
-        once: "1",
-        twice: "2",
-        thrice: "3",
-        infinite: "infinite",
-    },
-    animationFillMode: {
-        none: "none",
-        forwards: "forwards",
-        backwards: "backwards",
-        both: "both",
-    },
-    animationRange: {
-        normal: "normal",
-        cover: "cover",
-        contain: "contain",
-        entry: "entry",
-        exit: "exit",
-        gradual: "10% 90%",
-        moderate: "20% 80%",
-        brisk: "30% 70%",
-        rapid: "40% 60%",
-    },
     timeline: {
         none: "none",
         auto: "auto",
